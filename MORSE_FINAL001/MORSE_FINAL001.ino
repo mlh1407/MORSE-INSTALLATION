428251a4517c918447492b9039efb58bd450c682
Emil Lykke Diget <emil@lykke-diget.dk>
2017-10-03 18:47:54 +0200

    alternativ

--- MORSE_FINAL001/MORSE_FINAL001.ino
+++ MORSE_FINAL001/MORSE_FINAL001.ino
@@ -1,22 +1,22 @@
 /*
- * 
- */
- 
+
+*/
+
 #include <Adafruit_NeoPixel.h>
 
-#define PIXEL_PIN 1
+#define PIXEL_PIN 6
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(107, PIXEL_PIN, NEO_RGB + NEO_KHZ400);
 
-const long UNIT_TIME = 120; // dit duration in milliseconds4
+const long UNIT_TIME = 120; // dit duration in milliseconds
 
-const int morseKeyPin = 0;
+const int morseKeyPin = 5;
 // const int LED_PIN = 1; // lokalt visuelt feedback
 const int WORD_SIZE = 1;
 const int ALPHABET_SIZE = 46;
 int wait = 10;
 int words = 0;
 bool allow = true;
-char* message[WORD_SIZE]; 
+char* message[WORD_SIZE];
 char* letters[] = {
   ".-", "A",
   "-...", "B",
@@ -74,140 +74,182 @@ const int DITSLEN = 10;
 char dits[DITSLEN];
 int i = 0;
 bool firstTime = true;
-int numArrar1[]={98,97,96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60,59,58,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,9,8,7,6,5,4,3,2,1,0};
-//int numArrar1[]={96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,79,78,77,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,29,28,27,9,8,7,6,5,4,3,2,1,0};
-int numArrar2[]={10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60,59,58,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,97,98,99,100,101,102,103,104,105,106};
- 
+//int numArrar1[]={98,97,96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60,59,58,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,9,8,7,6,5,4,3,2,1,0};
+////int numArrar1[]={96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,79,78,77,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,29,28,27,9,8,7,6,5,4,3,2,1,0};
+//int numArrar2[]={10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60,59,58,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,97,98,99,100,101,102,103,104,105,106};
 
 void setup()
 {
   pinMode(morseKeyPin, INPUT_PULLUP);
-//  pinMode(LED_PIN, OUTPUT);
+  //  pinMode(LED_PIN, OUTPUT);
 
   prevtime = millis();
- 
+
   Serial.begin(9600); // debug skal væk
   //
   strip.begin();
+
+  for (int i = 0; i < 107; i++)
+  {
+    strip.setPixelColor(i, 0, 0, 0);
+  }
+  
   strip.show(); // Initialize all pixels to 'off'
 }
 
 void emitLetter() {
 
-  if (firstTime) {
-     
+  if (firstTime) 
+  {
     firstTime = false;
-   
   }
-  dits[i] = '\0';
+  
+  // dits[i] = '\0';
 
   String stringOne = dits;
-   
-  for (int j = 0; j < ALPHABET_SIZE; j++) {
-    
-    if (stringOne == letters[2 * j]) {
- 
-       Serial.println(letters[2 * j + 1]);
-       
-       message[curSentenceIndex]= letters[2 * j + 1 ];
-       
-       curSentenceIndex++;
+
+  for (int j = 0; j < ALPHABET_SIZE; j++) 
+  {
+    if (stringOne == letters[2 * j]) 
+    {
+      // Serial.println(letters[2 * j + 1]);
+      
+      message[curSentenceIndex] = letters[2 * j + 1 ];
+
+      curSentenceIndex++;
     }
-    
+//    else
+//    {
+//      Serial.println("Letter not recognized");
+//    }
+  }
 
+  for (int j = 0; j < WORD_SIZE; j++) 
+  {
+    Serial.println(message[j]);
   }
-  
-  for (int j = 0; j < WORD_SIZE; j++) {
-    Serial.println(message[i]);
-  }
-  
+
   i = 0;
-  if (words >= WORD_SIZE) {
+  
+  if (words >= WORD_SIZE) // Den er færdig med ordnene
+  {
     words = 0;
-    curSentenceIndex=0;
-    allow=false;
-
+    curSentenceIndex = 0;
+    allow = false;
+    
     sendMorseLeds();
+    
+    firstTime = true;
+    words = 0;
+    allow = true;
     //return;
-  } else {
+  } 
+  else 
+  {
     words++;
   }
 }
- 
-void sendMorseLeds() {
-  for (int i = 0; i < 107; i++) {
-    strip.setPixelColor(i, strip.Color(30, 0, 0));
+
+void sendMorseLeds() 
+{
+  Serial.println("Turning LEDs on");
+  
+  for (int j = 0; j < 107; j++) 
+  {
+    strip.setPixelColor(j, strip.Color(30, 0, 0));
     strip.show();
-    delay(wait*2);
-    strip.setPixelColor(i-2, strip.Color(0, 0, 0));
+    delay(wait * 2);
+    strip.setPixelColor(j - 2, strip.Color(0, 0, 0));
     strip.show();
     delay(wait);
   }
-  
-  for(int i =0;i<WORD_SIZE;i++){ 
-     Serial.println(message[i]);
-     delay(wait);
-  } 
-  
-  delay(3000);
-  
-  firstTime=true;
-  words=0;
-  allow=true;
+
+  for (int j = 0; j < WORD_SIZE; j++) 
+  {
+    Serial.println(message[j]);
+    delay(wait);
+  }
+
+  // delay(3000);
+  // delay(500);
 }
 
-void loop() {   
-  int reversed = 0;
-  if (digitalRead(morseKeyPin)) {
+void loop() {
+  int reversed;
+
+  if (digitalRead(morseKeyPin)) 
+  {
     reversed = 0;
-  } else {
+  } 
+  else 
+  {
     reversed = 1;
   }
   int state = reversed;
-  
+
   // digitalWrite(LED_PIN, state ? HIGH : LOW); // debug
 
   // Serial.print(state ? "1" : "0");
-  
+
   unsigned long t = millis();
   unsigned long len = t - prevtime;
 
-  if (prevstate != state) {
-    if (prevstate) {
-      if (len < 10) {
+  if (prevstate != state) 
+  {
+    if (prevstate) 
+    {
+      if (len < 10) 
+      {
         // ignore
-      } else if (len < UNIT_TIME * 2) {
+        // Det må være, hvis det er bounce
+      } 
+      else if (len < UNIT_TIME * 2) 
+      {
         // dit
-        if (i < DITSLEN - 1) {
+        if (i < DITSLEN - 1) 
+        {
           dits[i++] = '.';
-        
+
           Serial.print(".");
         }
-      } else {
+      } 
+      else 
+      {
         // dah
-        if (i < DITSLEN - 1) {
+        if (i < DITSLEN - 1) 
+        {
           dits[i++] = '-';
-        
-        Serial.print("-");
+
+          Serial.print("-");
         }
       }
-    } else {
-      if (len < UNIT_TIME * 2) {
+    } 
+    else 
+    {
+      if (len < UNIT_TIME * 2) 
+      {
         // intra-letter gap
-      } else if (len < UNIT_TIME * 5.33) {
-        // gap between letters  
+        //Serial.println("intra-letter gap");
+      } 
+      else if (len < UNIT_TIME * 5.33) 
+      {
+        // gap between letters
+        Serial.println(dits);
+        Serial.println("gap between letters");
         emitLetter();
       }
     }
 
     prevstate = state;
     prevtime = t;
-  } else {
-    if (!prevstate && (i > 0) && (len >= UNIT_TIME * 5.33)) {
+  } 
+  else 
+  {
+    if (!prevstate && (i > 0) && (len >= UNIT_TIME * 5.33)) 
+    {
       // gap between words
-     
-       // Serial.print(" ");
+
+      // Serial.print(" ");
     }
   }
-  
 }
---
+++ alternativ/keboard/keboard.ino
@@ -0,0 +1,215 @@
+// Turns Morse key into USB keyboard
+
+#include <Bounce.h> // include de-bounce library
+
+const int led = 13; // led is connected to pin 13
+const int keyPin = 0;  // morse key is connected to pin 7
+Bounce morseKey = Bounce(keyPin, 10);  // 10 ms debounce
+
+const unsigned long dashThresh = 150; // time threshold in ms to differentiate dots from dashes
+const unsigned long letterThresh = 500; // time threshold in ms to differentiate letter gaps
+const unsigned long wordThresh = 3000; // time threshold in ms to differentiate word gaps
+
+String inputString = ""; // initialise input string
+
+unsigned long downTime = 0; // records the start time of state change
+unsigned long upTime = 0; // records the end time of state change
+unsigned long timeNow = 0; // records the current time 
+unsigned long changeDuration = 0; // records the duration of state change
+unsigned long pauseDuration = 0; // records the duration of the last pause
+
+int pauseFlag = 0; // initilise the flag to indicate whether a pause has already been evaluated
+
+void setup()
+{
+  pinMode(led, OUTPUT); // configure the pin connected to the led as an output
+  pinMode(keyPin, INPUT_PULLUP); // configure the pin connected to the morse key as a pullup
+} // end of setup
+
+void loop()
+{ 
+  checkPause();
+  // start of IF loop
+  if (morseKey.update()){
+    
+    if (morseKey.risingEdge()) { // if input from key has gone to 1 and model is still 0, update model
+
+      keyUp();
+  
+    } else if (morseKey.fallingEdge()) { // if input from key has gone to 0 and model is still 1, update model
+  
+      keyDown();
+  
+    }
+  } // end of if update loop
+  
+} // end of loop
+
+void keyDown()
+{
+    downTime = millis();
+    digitalWrite(led, HIGH); // switch LED on
+}
+
+void keyUp()
+{
+    upTime = millis();
+    changeDuration = upTime-downTime; 
+    digitalWrite(led, LOW); // switch LED off
+
+    if (changeDuration>0 and changeDuration<dashThresh){
+      inputString = inputString + ".";
+      Serial.println("DOT");
+
+    } else if (changeDuration>=dashThresh) {
+      inputString = inputString + "-";
+      Serial.println("DASH");
+
+    }
+
+    pauseFlag = 1;
+    
+}
+
+void checkPause()
+{   
+    timeNow = millis();
+    pauseDuration = timeNow-upTime;
+
+    if (pauseDuration>=letterThresh and pauseDuration<wordThresh and pauseFlag){ // if the preceding pause was long enough AND a pause hasn't just been evaluated, evaluate the previous inputs as a single letter
+
+      evaluateLetter();
+      pauseFlag = 0;
+      
+    } else if (pauseDuration >= wordThresh and pauseFlag) {
+
+      evaluateLetter();
+      newWord();
+      pauseFlag = 0; 
+      
+    }
+}
+
+void newWord()
+{
+  Keyboard.press(KEY_SPACE);
+  Keyboard.release(KEY_SPACE);
+}
+
+void evaluateLetter()
+{
+  if (inputString==".-") {
+      Keyboard.press(KEY_A);
+      Keyboard.release(KEY_A);
+  } else if (inputString=="-..."){
+      Keyboard.press(KEY_B);
+      Keyboard.release(KEY_B);
+  } else if (inputString == "-.-."){
+      Keyboard.press(KEY_C);
+      Keyboard.release(KEY_C);
+  } else if (inputString=="-.."){
+      Keyboard.press(KEY_D);
+      Keyboard.release(KEY_D);
+  } else if (inputString=="."){
+      Keyboard.press(KEY_E);
+      Keyboard.release(KEY_E);
+  } else if (inputString=="..-."){
+      Keyboard.press(KEY_F);
+      Keyboard.release(KEY_F);
+  } else if (inputString=="--."){
+      Keyboard.press(KEY_G);
+      Keyboard.release(KEY_G);
+  } else if (inputString=="...."){
+      Keyboard.press(KEY_H);
+      Keyboard.release(KEY_H);
+  } else if (inputString==".."){
+      Keyboard.press(KEY_I);
+      Keyboard.release(KEY_I);
+  } else if (inputString==".---"){
+      Keyboard.press(KEY_J);
+      Keyboard.release(KEY_J);
+  } else if (inputString=="-.-"){
+      Keyboard.press(KEY_K);
+      Keyboard.release(KEY_K);
+  } else if (inputString==".-.."){
+      Keyboard.press(KEY_L);
+      Keyboard.release(KEY_L);
+  } else if (inputString=="--"){
+      Keyboard.press(KEY_M);
+      Keyboard.release(KEY_M);
+  } else if (inputString=="-."){
+      Keyboard.press(KEY_N);
+      Keyboard.release(KEY_N);
+  } else if (inputString=="---"){
+      Keyboard.press(KEY_O);
+      Keyboard.release(KEY_O);
+  } else if (inputString==".--."){
+      Keyboard.press(KEY_P);
+      Keyboard.release(KEY_P);
+  } else if (inputString=="--.-"){
+      Keyboard.press(KEY_Q);
+      Keyboard.release(KEY_Q);
+  } else if (inputString==".-."){
+      Keyboard.press(KEY_R);
+      Keyboard.release(KEY_R);
+  } else if (inputString=="..."){
+      Keyboard.press(KEY_S);
+      Keyboard.release(KEY_S);
+  } else if (inputString=="-"){
+      Keyboard.press(KEY_T);
+      Keyboard.release(KEY_T);
+  } else if (inputString=="..-"){
+      Keyboard.press(KEY_U);
+      Keyboard.release(KEY_U);
+  } else if (inputString=="...-"){
+      Keyboard.press(KEY_V);
+      Keyboard.release(KEY_V);
+  } else if (inputString==".--"){
+      Keyboard.press(KEY_W);
+      Keyboard.release(KEY_W);
+  } else if (inputString=="-..-"){
+      Keyboard.press(KEY_X);
+      Keyboard.release(KEY_X);
+  } else if (inputString=="-.--"){
+      Keyboard.press(KEY_Y);
+      Keyboard.release(KEY_Y);
+  } else if (inputString=="--.."){
+      Keyboard.press(KEY_Z);
+      Keyboard.release(KEY_Z);
+  } else if (inputString==".----"){
+      Keyboard.press(KEY_1);
+      Keyboard.release(KEY_1);
+  } else if (inputString=="..---"){
+      Keyboard.press(KEY_2);
+      Keyboard.release(KEY_2);
+  } else if (inputString=="...--"){
+      Keyboard.press(KEY_3);
+      Keyboard.release(KEY_3);
+  } else if (inputString=="....-"){
+      Keyboard.press(KEY_4);
+      Keyboard.release(KEY_4);
+  } else if (inputString=="....."){
+      Keyboard.press(KEY_5);
+      Keyboard.release(KEY_5);
+  } else if (inputString=="-...."){
+      Keyboard.press(KEY_6);
+      Keyboard.release(KEY_6);
+  } else if (inputString=="--..."){
+      Keyboard.press(KEY_7);
+      Keyboard.release(KEY_7);
+  } else if (inputString=="---.."){
+      Keyboard.press(KEY_8);
+      Keyboard.release(KEY_8);
+  } else if (inputString=="----."){
+      Keyboard.press(KEY_9);
+      Keyboard.release(KEY_9);
+  } else if (inputString=="-----"){
+      Keyboard.press(KEY_0);
+      Keyboard.release(KEY_0);
+  } else { 
+      Keyboard.press(KEY_MINUS);
+      Keyboard.release(KEY_MINUS);
+  }
+
+  inputString = ""; // re-initialise inputString ready for new letter
+}
